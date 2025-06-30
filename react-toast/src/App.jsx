3b0121d3dcc5a0e92cd679e8c57440146e1b5c60
Peter Abahim <lilpero03@gmail.com>
2025-06-30 01:16:06 +0100

    did the toast

--- react-toast/src/App.jsx
+++ react-toast/src/App.jsx
@@ -3,33 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
 
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  function info (){
+
+    toast.info("This is an info toast!");
+
+    toast.error("This is a error")
+
+  }
+  
+
+  const notify = () => toast.info("🦄 Wow! This is a toast!");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={info}>Show Toast</button>
+
+      {/* Must include ToastContainer to display toasts */}
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default App
+export default App;
